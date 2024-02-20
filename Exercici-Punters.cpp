addce7865cdda3036f0c84f7c72bc9fa8229d035
Minepika55 <98884543+Minepika55@users.noreply.github.com>
2024-02-20 12:59:54 +0100

    Add files via upload

--- Exercici-Punters.cpp
+++ Exercici-Punters.cpp
@@ -5,16 +5,58 @@ using namespace std;
 
 int main()
 {
-	int* punter1 = NULL;
-	punter1 = new int;
+	int* Punter1 = NULL;
+	cout << Punter1;
+
+	Punter1 = new int;
 	int opcio = 0;
 
-	cout << "Vols esborrar el valor " << punter1 << " ?" << endl;
+	cout << "Vols esborrar el Punter " << Punter1 << " ?" << endl;
 	cin >> opcio;
 
 	if (opcio == 1)
 	{
-		delete punter1;
+		delete Punter1;
+		cout << Punter1;
 	}
 
+	string Fruites[3] = { "Mandarina", "Maduixa", "Platan" };
+
+	//cout << "Vols esborrar l'Array " << Fruites << " ?" << endl;
+	//cin >> opcio;
+
+	//if (opcio == 1)
+	//{
+	//	delete Fruites;
+	//	cout << Fruites;
+	//}
+
+	cout << "Com de gran ha de ser l'array?"<< endl;
+
+	int llargaria;
+
+	cin >> llargaria;
+
+	int* arr = new int[llargaria];
+
+	for (int i = 0; i < llargaria; i++) {
+		arr[i] = (i + 1) * 10;
+	}
+
+	for (int i = 0; i < llargaria; i++) {
+		cout << arr[i] << " " << endl;
+	}
+
+	cout << "Aixi queda l'array: " << llargaria << " ?" << endl;
+
+	cout << "Vols esborrar l'Array " << llargaria << " ?" << endl;
+	cin >> opcio;
+
+	if (opcio == 1)
+	{
+		delete[] arr;
+		cout << llargaria;
+	}
+
+	return 0;
 }
